d44b2b143015132dbcc9f1c108f0a77b71ea80e5
ahmed adel <ahmedadel123422@gmail.com>
2025-01-05 22:40:27 +0200

    commit

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,41 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+
+  //* List document id
+  List<String> docIDs = [];
+
+  //* get docIDs
+  Future getDocIDs() async {
+    await FirebaseFirestore.instance.collection('user').get().then(
+          (snapshot) => snapshot.docs.forEach((document) {
+            print(document.reference);
+            docIDs.add(document.reference.id);
+          }),
+        );
+  }
+
+  @override
+  void initState() {
+    getDocIDs();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
